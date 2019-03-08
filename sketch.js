b176de2120990a02541c8325f8a7c043bdc92bbc
jordanherbert <40042885+jordanherbert@users.noreply.github.com>
2019-03-08 15:13:47 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,28 @@
 let can;
 let vst;
 let vend;
-let vmid;
-let vlho;
-let vrho;
 let vees=[];
-let input;
-let sum;
 
 function setup() {
 can=createCanvas(1100,500);
-can.position(50,50);
+can.position(100,100);
 can.class("bord");
 
 vst=createVector(width/2,height);
 vend=createVector(width/2,height/2);
 vees.push(new vclass(vst,vend));
 
-
 createburrito();
 
-
+	anch=select('#sier');
+	anch.position(0,0);
+	anch.class("bord");
 }
 
 function draw() {
 background(0);
 for(let j=0;j<vees.length;j++){
-	vees[j].show();//vees[j].giggles(1);
-	//vees[j].push(new vclass(vst,vend));
+	vees[j].show();
 drawburrito();
 }
 }
